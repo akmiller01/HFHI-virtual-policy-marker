ddac02604b5c31f0b82dc46be62c67046b06e535
akmiller01 <alex.k.miller@gmail.com>
2024-12-30 14:55:44 -0500

    Collate all datasets

--- code/1_download_crs.py
+++ code/1_download_crs.py
@@ -1,28 +1,47 @@
+import os
 from oda_reader import download_crs
+import pandas as pd
 
 # Download small subset first
-crs_sector_data = download_crs(
-    start_year=2014, 
-    end_year=2023, 
-    filters={
-        'sector': ['16030', '16040'],
-        'flow_type': 'D',
-        'price_base': 'Q',
-        'microdata': True
-    }, 
-    dataflow_version='1.3'
-)
-crs_sector_data.to_csv('input/crs_2014_2023_purpose_codes.csv')
+sector_filename = 'input/crs_2014_2023_purpose_codes.csv'
+if not os.path.exists(sector_filename):
+    crs_sector_data = download_crs(
+        start_year=2014, 
+        end_year=2023, 
+        filters={
+            'sector': ['16030', '16040'],
+            'flow_type': 'D',
+            'price_base': 'Q',
+            'microdata': True
+        }, 
+        dataflow_version='1.3'
+    )
+    crs_sector_data.to_csv(sector_filename)
 
 # Next, all disbursements
-crs_data = download_crs(
-    start_year=2014, 
-    end_year=2023, 
-    filters={
-        'flow_type': 'D',
-        'price_base': 'Q',
-        'microdata': True
-    }, 
-    dataflow_version='1.3'
-)
-crs_data.to_csv('large_input/crs_2014_2023.csv')
+for year in range(2014, 2024):
+    year_filename = 'large_input/crs_{}.csv'.format(year)
+    if not os.path.exists(year_filename):
+        crs_data = download_crs(
+            start_year=year, 
+            end_year=year, 
+            filters={
+                'flow_type': 'D',
+                'price_base': 'Q',
+                'microdata': True
+            }, 
+            dataflow_version='1.3'
+        )
+        crs_data.to_csv(year_filename)
+
+# Collate
+all_filename = 'large_input/crs_2014_2023.csv'
+if not os.path.exists(all_filename):
+    all_dataframes = []
+
+    for year in range(2014, 2024):
+        year_filename = 'large_input/crs_{}.csv'.format(year)
+        df = pd.read_csv(year_filename, encoding='utf-8')
+        all_dataframes.append(df)
+    concatenated_df = pd.concat(all_dataframes, ignore_index=True)
+    concatenated_df.to_csv(all_filename)
