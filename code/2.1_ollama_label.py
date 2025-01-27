6c16eb761548c2c5ec5da06ca2556296d9353b92
akmiller01 <alex.k.miller@gmail.com>
2025-01-26 23:18:54 -0500

    Break if not big definition

--- code/2.1_ollama_label.py
+++ code/2.1_ollama_label.py
@@ -1,8 +1,12 @@
 # curl -fsSL https://ollama.com/install.sh | sh
 # pip install datasets ollama
 
+# TODO:
+# 1. Convert urban/rural into one urban/rural/unspecified?
+# 2. Look into UNHABITAT trigger
+
 import json
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
 import ollama
 from ollama import chat
 from ollama import ChatResponse
@@ -22,16 +26,15 @@ SYSTEM_PROMPT = (
     "then giving your answer in the 'answer' key."
     )
 DEFINITIONS = {
-    "housing": "any activities relating to housing, except for project staff housing",
+    "housing": "any activities relating to housing, housing policy, tents for the homeless, encampments for the homeless, homeless shelters, emergency shelters, refugee shelters, refugee camps, temporary supportive housing, housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, neighborhood integration, community land trusts, cooperative housing, public housing, subsidized home-rental, subsidized mortgages, rent-to-own housing, or market-rate housing",
     "homelessness": "tents for the homeless, encampments for the homeless, or homeless shelters",
     "transitional": "emergency shelters, refugee shelters, refugee camps, or temporary supportive housing",
     "incremental": "housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, or neighborhood integration",
     "social": "community land trusts, cooperative housing, or public housing",
-    "market": "subsidized home-rental, first-time home buyer programs, rent-to-own housing, or market-rate housing",
+    "market": "subsidized home-rental, subsidized mortgages, rent-to-own housing, or market-rate housing",
     "urban": "activities in specific urban locations",
     "rural": "activities in specific rural locations",
-    "adaptation": "adapting to the direct effects of climate change",
-    "mitigation": "mitigating the direct causes of climate change"
+    "environment": "activities that relate to climate change adaptation, climate change mitigation, or environmental protection"
 }
 FORMAT = {
     "type": "object",
@@ -72,6 +75,11 @@ def ollama_label(example):
         for response_key in parsed_response_content:
             definition_response_key = f"{key}_{response_key}"
             example[definition_response_key] = parsed_response_content[response_key]
+        if key == 'housing' and parsed_response_content['answer'] == False:
+            for other_key in list(DEFINITIONS.keys())[1:]:
+                example[f"{other_key}_thoughts"] = ""
+                example[f"{other_key}_answer"] = False
+            break
 
     return example
 
@@ -83,8 +91,9 @@ def main():
     # Load data
     dataset = load_dataset('alex-miller/crs-2014-2023', split='train')
     dataset = dataset.add_column('id', range(dataset.num_rows))
-    dataset = dataset.filter(lambda example: example['sector_code'] in [16030, 16040])
-    dataset = dataset.shuffle(seed=1337).select(range(10))
+    pos = dataset.filter(lambda example: example['sector_code'] in [16030, 16040]).shuffle(seed=1337).select(range(5))
+    neg = dataset.filter(lambda example: example['sector_code'] not in [16030, 16040]).shuffle(seed=1337).select(range(5))
+    dataset = concatenate_datasets([neg, pos])
 
     # Label
     dataset = dataset.map(ollama_label)
