7e81a70bf23f73cbb942145e775ea8370fe5d73d
akmiller01 <alex.k.miller@gmail.com>
2025-01-28 15:26:40 -0500

    Back to gte

--- code/2.1_embedding_retrieval.py
+++ code/2.1_embedding_retrieval.py
@@ -3,6 +3,7 @@
 import os
 import torch
 from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
 from datasets import load_dataset
 from dotenv import load_dotenv
 from huggingface_hub import login
@@ -11,22 +12,20 @@ from huggingface_hub import login
 global DEVICE
 global MODEL
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-MODEL = SentenceTransformer("Alibaba-NLP/gte-Qwen2-1.5B-instruct", trust_remote_code=True, device=DEVICE)
-MODEL.max_seq_length = 8192
+MODEL = SentenceTransformer("Alibaba-NLP/gte-multilingual-base", device=DEVICE, trust_remote_code=True)
 
 
 def main(queries):
-    # Prepare a prompt given an instruction
-    instruction = 'Given a classification, decide whether the text matches the classification.'
-    prompt = f'Instruct: {instruction}\nQuery: '
     dataset = load_dataset('alex-miller/crs-2014-2023', split='train')
-    query_embeddings = MODEL.encode(queries, prompt=prompt)
-    text_embeddings = MODEL.encode(dataset["text"], batch_size=256, show_progress_bar=True)
+    text_embeddings = MODEL.encode(dataset["text"], batch_size=512, show_progress_bar=True, normalize_embeddings=True)
+    query_embeddings = MODEL.encode(queries, normalize_embeddings=True)
     similarities = MODEL.similarity(query_embeddings, text_embeddings)
+
     for i in range(0, len(queries)):
         query = queries[i]
         col_name = "{}_similarity".format(query.replace(' ', '_').replace('-', '_'))
         dataset = dataset.add_column(col_name, similarities[i,:].tolist())
+
     dataset.push_to_hub('alex-miller/crs-2014-2023-housing-similarity')
 
 
@@ -37,3 +36,5 @@ if __name__ == '__main__':
     query_str = "housing, housing policy, housing finance, habitability, homelessness, tents, encampments, shelters, emergency shelters, refugee shelters, temporary supportive housing, housing sites, housing services, housing technical assistance, slums, slum upgrading, housing structural repairs, neighborhood integration, community land trusts, cooperative housing, public housing, home-rental, homeownership, mortgages, rent-to-own housing, market-rate housing"
     queries = query_str.split(", ")
     main(queries)
+
+
