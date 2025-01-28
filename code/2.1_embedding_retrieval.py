177c7bd88c357e8242628505ce7a8345c1e58618
akmiller01 <alex.k.miller@gmail.com>
2025-01-28 10:35:59 -0500

    Prep for remote env

--- code/2.1_embedding_retrieval.py
+++ code/2.1_embedding_retrieval.py
@@ -1,11 +1,15 @@
+# ! pip install tqdm torch sentence-transformers numpy datasets python-dotenv huggingface_hub
+
+import os
 from tqdm import tqdm
 import torch
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.util import cos_sim
 import numpy as np
 from datasets import load_dataset
-from datasets.utils.logging import disable_progress_bar
-disable_progress_bar()
+from dotenv import load_dotenv
+from huggingface_hub import login
+
 
 global DEVICE
 global MODEL
@@ -15,7 +19,6 @@ MODEL = SentenceTransformer("Alibaba-NLP/gte-large-en-v1.5", device=DEVICE, trus
 
 def main(queries):
     dataset = load_dataset('alex-miller/crs-2014-2023', split='train')
-    dataset = dataset.shuffle(seed=1337).select(range(10))
     sentences = dataset["text"]
 
     file_embeddings = list()
@@ -35,10 +38,13 @@ def main(queries):
     dataset = dataset.add_column("housing_similarity", similarities)
     dataset = dataset.add_column("max_query", query_maxes)
 
-    dataset.to_csv("large_input/embedding_filtered_crs_2014_2023.csv")
+    dataset.push_to_hub('alex-miller/crs-2014-2023-housing-retrieval')
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    HF_TOKEN = os.getenv('HF_TOKEN')
+    login(token=HF_TOKEN)
     query_str = "housing, housing policy, housing finance, habitability, tents for the homeless, encampments for the homeless, homeless shelters, emergency shelters, refugee shelters, refugee camps, temporary supportive housing, housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, neighborhood integration, community land trusts, cooperative housing, public housing, subsidized home-rental, subsidized mortgages, rent-to-own housing, market-rate housing"
     queries = query_str.split(", ")
     main(queries)
