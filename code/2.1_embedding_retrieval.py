5805224ba42dd104ff3c287fa784260ac85fb653
akmiller01 <alex.k.miller@gmail.com>
2025-01-28 14:28:11 -0500

    Try Gemma 2"

--- code/2.1_embedding_retrieval.py
+++ code/2.1_embedding_retrieval.py
@@ -3,8 +3,6 @@
 import os
 import torch
 from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
-import numpy as np
 from datasets import load_dataset
 from dotenv import load_dotenv
 from huggingface_hub import login
@@ -13,25 +11,21 @@ from huggingface_hub import login
 global DEVICE
 global MODEL
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-MODEL = SentenceTransformer("Alibaba-NLP/gte-multilingual-base", device=DEVICE, trust_remote_code=True)
+MODEL = SentenceTransformer("BAAI/bge-multilingual-gemma2", model_kwargs={"torch_dtype": torch.float16}, device=DEVICE)
 
 
 def main(queries):
+    # Prepare a prompt given an instruction
+    instruction = 'Given a short topic, retrieve text that matches the topic.'
+    prompt = f'<instruct>{instruction}\n<query>'
     dataset = load_dataset('alex-miller/crs-2014-2023', split='train')
-
-    text_embeddings = MODEL.encode(dataset["text"], batch_size=512, show_progress_bar=True, normalize_embeddings=True)
-
-    query_embeddings = MODEL.encode(queries, normalize_embeddings=True)
-    similarities = np.zeros((len(text_embeddings), len(queries)))
-    for i, embedding in enumerate(text_embeddings):
-        similarity = cos_sim(query_embeddings, embedding)
-        similarities[i,:] = np.copy(similarity[:,0])
-
+    query_embeddings = MODEL.encode(queries, prompt=prompt)
+    text_embeddings = MODEL.encode(dataset["text"], batch_size=256, show_progress_bar=True)
+    similarities = MODEL.similarity(query_embeddings, text_embeddings)
     for i in range(0, len(queries)):
         query = queries[i]
         col_name = "{}_similarity".format(query.replace(' ', '_').replace('-', '_'))
         dataset = dataset.add_column(col_name, similarities[:,i])
-
     dataset.push_to_hub('alex-miller/crs-2014-2023-housing-similarity')
 
 
@@ -39,6 +33,6 @@ if __name__ == '__main__':
     load_dotenv()
     HF_TOKEN = os.getenv('HF_TOKEN')
     login(token=HF_TOKEN)
-    query_str = "accommodation, camps, Community Land Trusts, dwellings, encampments, habitat, home rental, homeless, homeownership, housing, informal settlements, market-rate housing, mortgage, neighborhood integration, rent-to-own, residential, settlements, shantytown, shelter, slum upgrading, slums, structural repairs, tents, urban development"
+    query_str = "housing, housing policy, housing finance, habitability, homelessness, tents, encampments, shelters, emergency shelters, refugee shelters, temporary supportive housing, housing sites, housing services, housing technical assistance, slums, slum upgrading, housing structural repairs, neighborhood integration, community land trusts, cooperative housing, public housing, home-rental, homeownership, mortgages, rent-to-own housing, market-rate housing"
     queries = query_str.split(", ")
     main(queries)
