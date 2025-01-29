b1e8af628dfc6e844c2e92a42d2e2e1e27104160
akmiller01 <alex.k.miller@gmail.com>
2025-01-29 08:58:15 -0500

    xgboost

--- code/2.1b_test_embed.py
+++ code/2.1b_test_embed.py
@@ -1,28 +1,23 @@
 import pandas as pd
 import re
 import numpy as np
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 # Load the data
-dataset = load_dataset("alex-miller/crs-2014-2023-housing-similarity", split="train").shuffle(seed=42).select(range(50000))
+dataset = load_dataset("alex-miller/crs-2014-2023-housing-similarity", split="train")
+pos = dataset.filter(lambda e: e['sector_code'] in [16030, 16040], num_proc=8)
+neg = dataset.filter(lambda e: e['sector_code'] not in [16030, 16040], num_proc=8).shuffle(seed=42).select(range(50000))
+dataset = concatenate_datasets([pos, neg])
 dat = dataset.to_pandas()
 
 # Load keywords
-keywords = pd.read_csv("~/git/HFHI-NLP/input/keywords.csv")
-
-# Filter keywords for relevant terms
-relevant_terms = ["housing", "informal settlement", "informal settlements", "shelter", "shelters", "slum", "slums"]
-keywords = keywords[keywords["literal_translation"].isin(relevant_terms)]
-
-# Filter for specific languages
-languages = ["English", "French", "Spanish"]
-keywords = keywords[keywords["language"].isin(languages)]
+keywords = pd.read_csv("input/keywords.csv")
 
 # Create regex pattern for keywords
 keyword_regex = "|".join([fr"\b{re.escape(word)}\b" for word in keywords["word"]])
@@ -38,6 +33,7 @@ dat["target"] = ((dat["sector_code"].isin([16030, 16040])) | dat["keyword"]).ast
 y = dat.pop('target').values.astype(int)
 dat.pop('text')
 dat.pop('sector_code')
+
 X = dat.values.astype(float)
 
 # Split
@@ -50,7 +46,7 @@ X_train = X_train[:,range(X_train.shape[1] -1)]
 X_test = X_test[:,range(X_test.shape[1] -1)]
 
 # Fit
-rf_class = RandomForestClassifier(n_estimators=100, random_state=0)
+rf_class = xgb.XGBClassifier(n_estimators=100)
 rf_class.fit(X_train, y_train)
 
 # Predict
@@ -63,11 +59,12 @@ f1 = f1_score(y_test, y_pred_rf)
 rec = recall_score(y_test, y_pred_rf)
 pre = precision_score(y_test, y_pred_rf)
 
-print("\nRandom Forest Classifier:")
+print("\nXGBoost Classifier:")
 print("Accuracy: ", acc)
 print("F1: ", f1)
 print("Recall: ", rec)
 print("Precision: ", pre)
+print(confusion_matrix(y_test, y_pred_rf))
 
 k_acc = accuracy_score(y_test, X_test_keyword)
 k_f1 = f1_score(y_test, X_test_keyword)
@@ -79,6 +76,8 @@ print("Accuracy: ", k_acc)
 print("F1: ", k_f1)
 print("Recall: ", k_rec)
 print("Precision: ", k_pre)
+print(confusion_matrix(y_test, X_test_keyword))
+
 
 both_acc = accuracy_score(y_test, rf_or_kw)
 both_f1 = f1_score(y_test, rf_or_kw)
@@ -90,6 +89,8 @@ print("Accuracy: ", both_acc)
 print("F1: ", both_f1)
 print("Recall: ", both_rec)
 print("Precision: ", both_pre)
+print(confusion_matrix(y_test, rf_or_kw))
+
 
 # Print the results
 cm = confusion_matrix(y_test, rf_or_kw)
