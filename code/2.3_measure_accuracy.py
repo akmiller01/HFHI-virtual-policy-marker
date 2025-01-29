65b2c9c84977d99fe1933a00e7f2bb342082a41c
akmiller01 <alex.k.miller@gmail.com>
2025-01-29 12:48:21 -0500

    Update accuracy measurement

--- code/2.3_measure_accuracy.py
+++ code/2.3_measure_accuracy.py
@@ -53,29 +53,32 @@ def main():
         with open(pickle_path, 'wb') as handle:
             pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)
     sample_size = 5
-    dataset = load_dataset("csv", data_files="large_input/crs_2014_2023_gpt_batched2.csv", split="train")
+    dataset = load_dataset("alex-miller/crs-2014-2023-housing-labeled", split="train")
     dimension_dict = {
-        "housing_general": "The user text describes any activities relating to housing.",
-        "homelessness_support":  "The user text describes support for homelessness, including tents, encampments, and homeless shelters.",
-        "transitional_housing": "The user text describes transitional housing, including emergency shelters, refugee shelters, camps and semi-permanent supportive housing.",
-        "incremental_housing": "The user text describes incremental housing, including housing sites, housing services and housing technical assistance, slum upgrading and structural repairs, and neighborhood integration.",
-        "social_housing": "The user text describes social housing, including community land trusts, cooperative housing, and public housing.",
-        "market_rent_own_housing": "The user text describes market-based housing solutions or rent-to-own housing policies, including social rental and subsidized rental, supported homeownership (first-time homebuyers and rent-to-own housing), and market-rate affordable housing.",
-        "urban": "The user text describes activities in urban settings in particular.",
-        "rural": "The user text describes activities in rural settings in particular.",
-        "climate_adaptation": "The user text describes activities relating to climate adaptation (responding to the effects of climate change or making communities more resilient to climate change).",
-        "climate_mitigation": "The user text describes activities relating to climate mitigation (reducing emissions or other causes of climate change).",
+        "housing": "directly or indirectly relates to any of the following: housing, housing policy, housing finance, habitability, tents for the homeless, encampments for the homeless, homeless shelters, emergency shelters, refugee shelters, refugee camps, temporary supportive housing, housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, neighborhood integration, community land trusts, cooperative housing, public housing, subsidized home-rental, subsidized mortgages, rent-to-own housing, or market-rate housing",
+        "homelessness": "explicitly describes tents for the homeless, encampments for the homeless, or homeless shelters",
+        "transitional": "explicitly describes emergency shelters, refugee shelters, refugee camps, or temporary supportive housing",
+        "incremental": "explicitly describes housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, or neighborhood integration",
+        "social": "explicitly describes community land trusts, cooperative housing, or public housing",
+        "market": "explicitly describes subsidized home-rental, subsidized mortgages, rent-to-own housing, or market-rate housing",
+        "urban_rural": "explicitly describes activities in specific urban locations, specific rural locations, both urban and rural locations, or neither"
     }
 
     for key in dimension_dict.keys():
+        thoughts_key = f'{key}_thoughts'
+        answer_key = f'{key}_answer'
         if key not in cache.keys():
             cache[key] = dict()
         print(f"\n\nColumn: {key}")
         definition = dimension_dict[key]
         print(f"Definition: {definition}")
 
-        pos = dataset.filter(lambda example: example[key] == 1)
-        neg = dataset.filter(lambda example: example[key] == 0)
+        if key == "urban_rural":
+            pos = dataset.filter(lambda example: example[answer_key] in ['Urban', 'Rural', 'Both'])
+            neg = dataset.filter(lambda example: example[answer_key] == 'Neither')
+        else:
+            pos = dataset.filter(lambda example: example[answer_key] == True)
+            neg = dataset.filter(lambda example: example[answer_key] == False)
 
         pos_pop = pos.num_rows
         neg_pop = neg.num_rows
@@ -90,7 +93,8 @@ def main():
             if key in cache.keys() and text in cache[key].keys():
                 manual_entry = cache[key][text]
             else:
-                print(f"{i + 1}. {text}")
+                thoughts = pos_sample[thoughts_key][i]
+                print(f"{i + 1}. {text}\nModel thoughts: {thoughts}")
                 try:
                     manual_entry = int(click.confirm(key, default=True))
                     cache[key][text] = manual_entry
@@ -99,8 +103,8 @@ def main():
                 except click.exceptions.Abort:
                     pos_sample_size -= 1
                     continue
-            automated_entry = pos_sample[key][i]
-            pos_corrects.append(int(manual_entry == automated_entry))
+            pos_corrects.append(int(manual_entry == True))
+
 
         pos_sample_accuracy = np.mean(pos_corrects).tolist()
         pos_sample_accuracy_round = round(pos_sample_accuracy * 100, 1)
@@ -114,7 +118,8 @@ def main():
             if key in cache.keys() and text in cache[key].keys():
                 manual_entry = cache[key][text]
             else:
-                print(f"{i + 1}. {text}")
+                thoughts = neg_sample[thoughts_key][i]
+                print(f"{i + 1}. {text}\nModel thoughts: {thoughts}")
                 try:
                     manual_entry = int(click.confirm(key, default=False))
                     cache[key][text] = manual_entry
@@ -124,7 +129,7 @@ def main():
                     neg_sample_size -= 1
                     continue
             automated_entry = neg_sample[key][i]
-            neg_corrects.append(int(manual_entry == automated_entry))
+            neg_corrects.append(int(manual_entry == False))
 
         neg_sample_accuracy = np.mean(neg_corrects).tolist()
         neg_sample_accuracy_round = round(neg_sample_accuracy * 100, 1)
