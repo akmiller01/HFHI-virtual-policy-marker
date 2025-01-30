78ef2ef83faee342abc9dcfdb7521bd9a2c665cd
akmiller01 <alex.k.miller@gmail.com>
2025-01-29 20:53:48 -0500

    Some prompt modifications and no more filtering

--- code/2.2_ollama_label.py
+++ code/2.2_ollama_label.py
@@ -7,6 +7,8 @@ from datasets import load_dataset
 import ollama
 from ollama import chat
 from ollama import ChatResponse
+from pydantic import BaseModel
+from enum import Enum
 from huggingface_hub import login
 from dotenv import load_dotenv
 
@@ -17,68 +19,48 @@ MODEL = "mistral:7b-instruct-v0.3-q3_K_M"
 global SYSTEM_PROMPT
 global URBAN_SYSTEM_PROMPT
 global DEFINITIONS
-global FORMAT
-global URBAN_FORMAT
 SYSTEM_PROMPT = (
     "You are a helpful assistant that classifies text. "
     "You are classifying whether the text {}. "
-    "You respond in JSON format, first giving thoughts in as few words as needed about whether the text matches the definition above in the 'thoughts' key and "
+    "Do not jump to conclusions: ground your response on the given text. "
+    "You respond in JSON format, first giving your thoughts about whether the text matches the definition above in the 'thoughts' key and "
     "then giving your answer in the 'answer' key."
 )
 URBAN_SYSTEM_PROMPT = (
     "You are a helpful assistant that classifies text. "
     "You are classifying whether the text explicitly describes activities in specific urban locations, specific rural locations, both urban and rural locations, or neither."
-    "You respond in JSON format, first giving thoughts in as few words as needed about whether the text matches the definitions above in the 'thoughts' key and "
+    "Do not jump to conclusions: ground your response on the given text. "
+    "You respond in JSON format, first giving your thoughts about whether the text matches the definitions above in the 'thoughts' key and "
     "then giving your answer in the 'answer' key. Possible answer choices are 'Urban', 'Rural', 'Both', or 'Neither'."
 )
 DEFINITIONS = {
-    "housing": "directly or indirectly relates to any of the following: housing, housing policy, housing finance, habitability, tents for the homeless, encampments for the homeless, homeless shelters, emergency shelters, refugee shelters, refugee camps, temporary supportive housing, housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, neighborhood integration, community land trusts, cooperative housing, public housing, subsidized home-rental, subsidized mortgages, rent-to-own housing, or market-rate housing",
+    "housing": "describes the housing sector, including but not limited to: provision of housing, provision of shelter in emergencies, improving the quality of life in inadequate housing, construction of housing, urban development, housing policy, technical assistance for housing, or finance for housing",
     "homelessness": "explicitly describes tents for the homeless, encampments for the homeless, or homeless shelters",
     "transitional": "explicitly describes emergency shelters, refugee shelters, refugee camps, or temporary supportive housing",
     "incremental": "explicitly describes housing sites, housing services, housing technical assistance, slum upgrading, housing structural repairs, or neighborhood integration",
     "social": "explicitly describes community land trusts, cooperative housing, or public housing",
-    "market": "explicitly describes subsidized home-rental, subsidized mortgages, rent-to-own housing, or market-rate housing",
+    "market": "explicitly describes home-rental, mortgages, rent-to-own housing, or market-rate housing",
 }
-FORMAT = {
-    "type": "object",
-    "properties": {
-        "thoughts": {
-            "type": "string"
-        },
-        "answer": {
-            "type": "boolean"
-        }
-    },
-    "required": [
-        "thoughts",
-        "answer"
-    ]
-}
-URBAN_FORMAT = {
-    "type": "object",
-    "properties": {
-        "thoughts": {
-            "type": "string"
-        },
-        "answer": {
-            "type": "string"
-        }
-    },
-    "required": [
-        "thoughts",
-        "answer"
-    ]
-}
+class ThoughtfulClassification(BaseModel):
+    thoughts: str
+    answer: bool
+class LocationType(Enum):
+    U = 'Urban'
+    R = 'Rural'
+    B = 'Both'
+    N = 'Neither'
+class ThoughtfulLocationClassification(BaseModel):
+    thoughts: str
+    answer: LocationType
 
 
 def ollama_label(example):
-    housing_sector = example['sector_code'] in [16030, 16040]
     for key in DEFINITIONS.keys():
         definition = DEFINITIONS[key]
         definition_system_prompt = SYSTEM_PROMPT.format(definition)
         response: ChatResponse = chat(
             model=MODEL,
-            format=FORMAT,
+            format=ThoughtfulClassification.model_json_schema(),
             messages=[
                 {
                     'role': 'system',
@@ -94,33 +76,25 @@ def ollama_label(example):
         for response_key in parsed_response_content:
             definition_response_key = f"{key}_{response_key}"
             example[definition_response_key] = parsed_response_content[response_key]
-        if example['housing_answer'] == False and housing_sector == False:
-            for other_key in list(DEFINITIONS.keys())[1:]:
-                example[f"{other_key}_thoughts"] = ""
-                example[f"{other_key}_answer"] = False
-            example["urban_rural_thoughts"] = ""
-            example["urban_rural_answer"] = "Neither"
-            break
-    if example['housing_answer'] == True or housing_sector == True:
-        key = "urban_rural"
-        response: ChatResponse = chat(
-            model=MODEL,
-            format=URBAN_FORMAT,
-            messages=[
-                {
-                    'role': 'system',
-                    'content': URBAN_SYSTEM_PROMPT,
-                },
-                {
-                    'role': 'user',
-                    'content': example['text'],
-                },
-            ]
-        )
-        parsed_response_content = json.loads(response.message.content)
-        for response_key in parsed_response_content:
-            definition_response_key = f"{key}_{response_key}"
-            example[definition_response_key] = parsed_response_content[response_key]
+    key = "urban_rural"
+    response: ChatResponse = chat(
+        model=MODEL,
+        format=ThoughtfulLocationClassification.model_json_schema(),
+        messages=[
+            {
+                'role': 'system',
+                'content': URBAN_SYSTEM_PROMPT,
+            },
+            {
+                'role': 'user',
+                'content': example['text'],
+            },
+        ]
+    )
+    parsed_response_content = json.loads(response.message.content)
+    for response_key in parsed_response_content:
+        definition_response_key = f"{key}_{response_key}"
+        example[definition_response_key] = parsed_response_content[response_key]
 
     return example
 
